11cef9d10873d745c570c3bef05fdf536e184aac
MGreizis <89737867+MGreizis@users.noreply.github.com>
2021-11-18 10:58:53 +0100

    part 2 final

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,31 @@
-const randomWords: string[] = ['random', 'school', 'hippopotamus', 'water', 'hangman', 'teacher', 'cryptocurrency', 'bitcoin', 'Albert Heijn'];
+const randomWords: string[] = ['random', 'school', 'hippopotamus', 'water', 'hangman', 'teacher', 'cryptocurrency', 'bitcoin', 'AlbertHeijn'];
 let word: string;
-const guessedCharactersInWord: string[] = [];
 let charactersInWord: string[];
-const charactersInDOM: HTMLDivElement = document.querySelector('#letters');
+const guessedcharactersInWord: string[] = [];
+const lettersInDOM: HTMLDivElement = document.querySelector('#letters');
 const attemptInDOM: HTMLDivElement = document.querySelector('#attempt');
-const attempts = 5;
+let attempts = 5;
 
 /**
- * Add dashes to the guessedCharacters in word.
+ * Function to split a word in Characters and replace it with dashes.
  */
 function splitWordInCharacters() {
   charactersInWord = word.split('');
-  console.log(charactersInWord);
+  // push - to another array where the guessed letters are stored, begin with dashes
   for (let i = 0; i < word.length; i += 1) {
-    guessedCharactersInWord.push('-');
+    guessedcharactersInWord.push('-');
   }
 }
 
 /**
- * Function that finds the index of characters in array
- *
- * @param target
- * @returns indexLocation
+ * Function to add a clicked letter to the GuessedWord array
+ * @param {numnber array} indexArray
+ * @param {string} letter
  */
-function findCharsInArray(target: HTMLElement) {
-  const indexLocation: number[] = [];
-  for (let i = 0; i < charactersInWord.length; i += 1) {
-    if (target.id === charactersInWord[i]) {
-      console.log(charactersInWord[i]);
-      indexLocation.push(i);
-    }
-  }
-  // console.log(indexLocation);
-  return indexLocation;
-}
-
-/**
- * Function to set a new word
- *
- * @param {string} newWord - a newly chosen word
- */
-function setWord(newWord: string) {
-  word = newWord;
+function addLetterToGuessedWord(indexArray: number[], letter: string) {
+  indexArray.forEach((charIndex) => {
+    guessedcharactersInWord[charIndex] = letter;
+  });
 }
 
 /**
@@ -52,30 +36,90 @@ function writeAttemptToTheDOM() {
 }
 
 /**
- * Function to write the guessed letters to the DOM
+ *
+ * @param {string} clickedLetter - the letter that the user clicked
+ * @returns {number[]} - the index in the array of the clicked letter
+ */
+function findLetters(clickedLetter: string): number[] {
+  // on what index is the letter
+  const indexOfLetters: number[] = [];
+  charactersInWord.forEach((letterInArray: string, index: number) => {
+    if (clickedLetter === letterInArray) {
+      indexOfLetters.push(index);
+    }
+  });
+  return indexOfLetters;
+}
+/**
+ * Function to set a new word
+ * @param {string} newWord - a newly chosen word
+ */
+function setWord(newWord: string) {
+  word = newWord;
+}
+
+/**
+ * Function to check if the users guessed the word right
+ */
+function checkWinner() {
+  console.log(`${word} is ${guessedcharactersInWord.join('')}`);
+  if (word === guessedcharactersInWord.join('')) {
+    lettersInDOM.classList.add('winner');
+  } else if (attempts === 0) {
+    lettersInDOM.classList.add('lost');
+    const keys = document.querySelectorAll('.key');
+    keys.forEach((key) => {
+      key.classList.add('idle');
+    });
+  }
+}
+
+/**
+ * Function to write the ghuessed letters to the DOM
  */
 function writeGuessedWordToTheDOM() {
-  charactersInDOM.innerHTML = '';
-  guessedCharactersInWord.forEach((letter) => {
+  lettersInDOM.innerHTML = '';
+  guessedcharactersInWord.forEach((letter) => {
     console.log(letter);
-    const li: HTMLLIElement = document.createElement('li');
+    const li = document.createElement('li');
     li.innerText = letter;
-    charactersInDOM.append(li);
+    lettersInDOM.append(li);
   });
 }
 
 /**
  * Function to handle the click event
- *
  * @param e {event} - click event
  */
 function guessLetter(e: Event) {
   // the target element where the user clicked
   const target: HTMLElement = e.target as HTMLElement;
   // the letter where the user clicked on
-  const letter: string = target.id;
-  console.log(letter);
-  findCharsInArray(target);
+  // let letter: string = target.id;
+  // console.log(target.className);
+  // check to see if the letter (and not some other element) is clicked on
+  if (target.className === 'key') {
+    console.log(target.id);
+    // find the indexes of all the occurences of the letter(s) in the string array (word)
+    const indexes: number[] = findLetters(target.id);
+    console.log(indexes);
+    // if the letter is found in the word
+    if (indexes.length !== 0) {
+      console.log('found');
+      // add the letter to the guessed word
+      addLetterToGuessedWord(indexes, target.id);
+      // make the chosen letter idle, you can not click on it any more
+      document.getElementById(target.id).classList.add('idle');
+    } else {
+      console.log('not found');
+      attempts -= 1;
+      writeAttemptToTheDOM();
+    }
+    // check if there is a winner
+    checkWinner();
+    // write the guessed letters to the DOM
+    writeGuessedWordToTheDOM();
+  }
 }
 
 /**
@@ -84,9 +128,9 @@ function guessLetter(e: Event) {
 function writeAlphabetToTheDom() {
   const alphabet: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
   const keyboard: HTMLDivElement = document.querySelector('#keyboard');
+  keyboard.addEventListener('click', guessLetter);
   alphabet.forEach((element) => {
     const divKey: HTMLDivElement = document.createElement('div');
-    divKey.addEventListener('click', guessLetter);
     divKey.id = element;
     divKey.classList.add('key');
     divKey.innerHTML = element;
@@ -95,12 +139,11 @@ function writeAlphabetToTheDom() {
 }
 
 /**
- * Returns a random number between min and max
- *
- * @param {number} min - lower boundary
- * @param {number} max - upper boundary
- * @returns {number} returns a random number
- */
+   * Returns a random number between min and max
+   * @param {number} min - lower boundary
+   * @param {number} max - upper boundary
+   * @returns {number} returns a number
+   */
 function randomNumber(min: number, max: number): number {
   return Math.round(Math.random() * (max - min) + min);
 }
@@ -114,8 +157,14 @@ function init() {
   // choose a word
   setWord(randomWords[randomNumber(0, randomWords.length)]);
   console.log(word);
+  // transform the word into an array of strings (letters)
   splitWordInCharacters();
+  // some debugging
+  console.log(word);
+  console.log(guessedcharactersInWord);
+  // write the amount of attempts to the DOM
   writeAttemptToTheDOM();
+  // write the letters in the guessed word array to the DOM
   writeGuessedWordToTheDOM();
 }
 
