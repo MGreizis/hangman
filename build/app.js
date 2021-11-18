b5126c72ea1ced398e85f5c4b23adf228af7c303
MGreizis <89737867+MGreizis@users.noreply.github.com>
2021-11-18 10:59:19 +0100

    part 2 final

--- build/app.js
+++ build/app.js
@@ -1,54 +1,84 @@
-const randomWords = ['random', 'school', 'hippopotamus', 'water', 'hangman', 'teacher', 'cryptocurrency', 'bitcoin', 'Albert Heijn'];
+const randomWords = ['random', 'school', 'hippopotamus', 'water', 'hangman', 'teacher', 'cryptocurrency', 'bitcoin', 'AlbertHeijn'];
 let word;
-const guessedCharactersInWord = [];
 let charactersInWord;
-const charactersInDOM = document.querySelector('#letters');
+const guessedcharactersInWord = [];
+const lettersInDOM = document.querySelector('#letters');
 const attemptInDOM = document.querySelector('#attempt');
-const attempts = 5;
+let attempts = 5;
 function splitWordInCharacters() {
     charactersInWord = word.split('');
-    console.log(charactersInWord);
     for (let i = 0; i < word.length; i += 1) {
-        guessedCharactersInWord.push('-');
+        guessedcharactersInWord.push('-');
     }
 }
-function findCharsInArray(target) {
-    const indexLocation = [];
-    for (let i = 0; i < charactersInWord.length; i += 1) {
-        if (target.id === charactersInWord[i]) {
-            console.log(charactersInWord[i]);
-            indexLocation.push(i);
-        }
-    }
-    return indexLocation;
-}
-function setWord(newWord) {
-    word = newWord;
+function addLetterToGuessedWord(indexArray, letter) {
+    indexArray.forEach((charIndex) => {
+        guessedcharactersInWord[charIndex] = letter;
+    });
 }
 function writeAttemptToTheDOM() {
     attemptInDOM.innerHTML = String(attempts);
 }
+function findLetters(clickedLetter) {
+    const indexOfLetters = [];
+    charactersInWord.forEach((letterInArray, index) => {
+        if (clickedLetter === letterInArray) {
+            indexOfLetters.push(index);
+        }
+    });
+    return indexOfLetters;
+}
+function setWord(newWord) {
+    word = newWord;
+}
+function checkWinner() {
+    console.log(`${word} is ${guessedcharactersInWord.join('')}`);
+    if (word === guessedcharactersInWord.join('')) {
+        lettersInDOM.classList.add('winner');
+    }
+    else if (attempts === 0) {
+        lettersInDOM.classList.add('lost');
+        const keys = document.querySelectorAll('.key');
+        keys.forEach((key) => {
+            key.classList.add('idle');
+        });
+    }
+}
 function writeGuessedWordToTheDOM() {
-    charactersInDOM.innerHTML = '';
-    guessedCharactersInWord.forEach((letter) => {
+    lettersInDOM.innerHTML = '';
+    guessedcharactersInWord.forEach((letter) => {
         console.log(letter);
         const li = document.createElement('li');
         li.innerText = letter;
-        charactersInDOM.append(li);
+        lettersInDOM.append(li);
     });
 }
 function guessLetter(e) {
     const target = e.target;
-    const letter = target.id;
-    console.log(letter);
-    findCharsInArray(target);
+    if (target.className === 'key') {
+        console.log(target.id);
+        const indexes = findLetters(target.id);
+        console.log(indexes);
+        if (indexes.length !== 0) {
+            console.log('found');
+            addLetterToGuessedWord(indexes, target.id);
+            document.getElementById(target.id).classList.add('idle');
+        }
+        else {
+            console.log('not found');
+            attempts -= 1;
+            writeAttemptToTheDOM();
+        }
+        checkWinner();
+        writeGuessedWordToTheDOM();
+    }
 }
 function writeAlphabetToTheDom() {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     const keyboard = document.querySelector('#keyboard');
+    keyboard.addEventListener('click', guessLetter);
     alphabet.forEach((element) => {
         const divKey = document.createElement('div');
-        divKey.addEventListener('click', guessLetter);
         divKey.id = element;
         divKey.classList.add('key');
         divKey.innerHTML = element;
@@ -63,6 +93,8 @@ function init() {
     setWord(randomWords[randomNumber(0, randomWords.length)]);
     console.log(word);
     splitWordInCharacters();
+    console.log(word);
+    console.log(guessedcharactersInWord);
     writeAttemptToTheDOM();
     writeGuessedWordToTheDOM();
 }
